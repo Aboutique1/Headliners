b1c47e1023b1a04b530f5458b173a9d39764d5c5
AmberWebb <amberrw@yahoo.com>
2019-10-16 15:46:01 -0700

    Dyanmically passed data from Node to Hanldebars Installed

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ app.engine('handlebars',exphbs());
 
 app.set('view engine','handlebars');
 
-app.get('/',function(req,res){res.render('home')});
+app.get('/',function(req,res){
+    
+    res.render('home',{title:"AmWebb"})
+
+});
 
 app.listen(3005,function(){
     console.log("I'm sleepy and hungry");
