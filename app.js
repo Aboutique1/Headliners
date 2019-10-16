06b7be284c6307dcfd8226847b49b2eed8f138b5
AmberWebb <amberrw@yahoo.com>
2019-10-16 16:12:41 -0700

    made a request with axios

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+
 var app = express();
 app.engine('handlebars',exphbs());
 
 app.set('view engine','handlebars');
 
 app.get('/',function(req,res){
+  axios.get('https://www.nastygal.com')
+      .then((response) => {
+
+        console.log(response)
+      })  
     
-    res.render('home',{title:"AmWebb"})
+    res.render('home',{title:"Headliners"})
 
 });
 
 app.listen(3005,function(){
     console.log("I'm sleepy and hungry");
 });
+
